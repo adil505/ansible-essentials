7c6992da365f03181aa0c1bf2e69db845a0f4dce
adil505 <38528259+adil505@users.noreply.github.com>
2020-02-18 08:53:32 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+    <html>
+
+
+    <head>
+
+    <title>Ansible Essentials</title>
+
+    </head>
+
+    <body>
+
+    <h1>Welcome to the world of cats! Brought to you from Github</h1>
+    <p> App backend <?=gethostname()?> </p>
+
+    <img src="https://loremflickr.com/320/240" />
+
+    </body>
+    </html>
